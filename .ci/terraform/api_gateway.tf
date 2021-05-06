6badaf6f625ea82ceea0bdb1688933c6e92b9090
Aram <aram_haroyan@epam.com>
2021-05-07 01:26:08 +0400

    Update api gateway

--- .ci/terraform/api_gateway.tf
+++ .ci/terraform/api_gateway.tf
@@ -2,25 +2,48 @@ resource "aws_api_gateway_rest_api" "api" {
   name = "announcements_api"
 }
 
-resource "aws_api_gateway_resource" "resource" {
-  path_part   = "resource"
+resource "aws_api_gateway_resource" "resource_get" {
+  path_part   = "resource_get"
   parent_id   = aws_api_gateway_rest_api.api.root_resource_id
   rest_api_id = aws_api_gateway_rest_api.api.id
 }
 
-resource "aws_api_gateway_method" "method" {
+resource "aws_api_gateway_resource" "resource_post" {
+  path_part   = "resource_post"
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.api.id
+}
+
+resource "aws_api_gateway_method" "get" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.resource.id
+  resource_id   = aws_api_gateway_resource.resource_get.id
   http_method   = "GET"
   authorization = "NONE"
 }
 
 
-resource "aws_api_gateway_integration" "integration" {
+resource "aws_api_gateway_method" "post" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.resource_post.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+
+resource "aws_api_gateway_integration" "integration_post" {
   rest_api_id             = aws_api_gateway_rest_api.api.id
-  resource_id             = aws_api_gateway_resource.resource.id
-  http_method             = aws_api_gateway_method.method.http_method
+  resource_id             = aws_api_gateway_resource.resource_post.id
+  http_method             = aws_api_gateway_method.post.http_method
   integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.lambda.invoke_arn
+}
+
+resource "aws_api_gateway_integration" "integration_get" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.resource_get.id
+  http_method             = aws_api_gateway_method.get.http_method
+  integration_http_method = "GET"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.lambda.invoke_arn
 }
@@ -33,7 +56,7 @@ resource "aws_lambda_permission" "apigw_lambda" {
   principal     = "apigateway.amazonaws.com"
 
   # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
-  source_arn = "arn:aws:execute-api:us-east-2:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.resource.path}"
+  source_arn = "arn:aws:execute-api:us-east-2:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.api.id}/*"
 }
 
 resource "aws_lambda_function" "lambda" {
